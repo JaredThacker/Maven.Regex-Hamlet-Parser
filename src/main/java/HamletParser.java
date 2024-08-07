8f2b07058b6354967ec905ae6dc2198ccc738fe0
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-08-07 12:03:53 -0400

    replace working

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,9 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +39,34 @@ public class HamletParser {
         return hamletData;
     }
 
+    public static void writeFile(String s){
+        try {
+            FileWriter fileWriter = new FileWriter("newHamlet.txt");
+            fileWriter.write(s);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String replaceHamlet(String text){
+        Pattern p = Pattern.compile("hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(text);
+        String newText = m.replaceAll("Leon");
+        return newText;
+    }
+
+    public static String replaceHoratio(String text){
+        Pattern p = Pattern.compile("horatio", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(text);
+        String newText = m.replaceAll("Tariq");
+        return newText;
+    }
+
+    public static void main(String[] args) {
+        HamletParser hamletParser = new HamletParser();
+        String hamletText = hamletParser.getHamletData();
+        String hamletReplaced = replaceHamlet(hamletText);
+        String horatioReplaced = replaceHoratio(hamletReplaced);
+        writeFile(horatioReplaced);
+    }
 }
